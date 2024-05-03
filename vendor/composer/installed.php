02c66496f8b024f35dfae57c6cc6b37c05736bef
insurgeNT@home <antaNT.pro@gmail.com>
2024-05-04 00:22:03 +0300

    compose jstree(whith jquery) datatables

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -19,6 +19,24 @@
             'aliases' => array(),
             'dev_requirement' => false,
         ),
+        'components/jquery' => array(
+            'pretty_version' => 'v3.7.1',
+            'version' => '3.7.1.0',
+            'reference' => '8edc7785239bb8c2ad2b83302b856a1d61de60e7',
+            'type' => 'component',
+            'install_path' => __DIR__ . '/../components/jquery',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'datatables/datatables' => array(
+            'pretty_version' => '1.10.21',
+            'version' => '1.10.21.0',
+            'reference' => '83e59694a105225ff889ddfa0d723a3ab24fda78',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../datatables/datatables',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
         'smarty/smarty' => array(
             'pretty_version' => 'v5.1.0',
             'version' => '5.1.0.0',
@@ -60,6 +78,15 @@
             'replaced' => array(
                 0 => 'v5.3.3',
             ),
+        ),
+        'vakata/jstree' => array(
+            'pretty_version' => '3.3.16',
+            'version' => '3.3.16.0',
+            'reference' => 'b3135cfc65eba90d4c670e6959273ef92cd1b447',
+            'type' => 'component',
+            'install_path' => __DIR__ . '/../vakata/jstree',
+            'aliases' => array(),
+            'dev_requirement' => false,
         ),
     ),
 );
