83e496a944013382d8cfb11abde7d65fe0a4f2cd
antaNT64pro@DellWorkBook <antaNT.pro@gmail.com>
2024-05-07 16:02:58 +0300

    datatables 2.0.7 работает

--- admin/js/index.js
+++ admin/js/index.js
@@ -3,8 +3,10 @@ import * as ui from './ui/uiAdmin.js';
 import './ui/uiEvents.js';
 import * as bsToast from "../apps/Toasts/appToasts.js";
 
+// import DataTable from './node_modules/datatables.net-bs5/js/dataTables.bootstrap5.mjs';
+// d:/OSPanel/domains/antCMS.os/admin/js/node_modules/datatables.net-bs5/js/dataTables.bootstrap5.js
 
-console.log(ui)
+// console.log(ui)
 let today = new Date();
 // ui.elTemplateNow.innerHTML = formatTime(today);
 
