d17ff38f347058623f6af6a921d005232cd31ad9
antaNT64pro@DellWorkBook <antaNT.pro@gmail.com>
2024-05-08 21:48:49 +0300

    q

--- admin/admin.php
+++ admin/admin.php
@@ -345,7 +345,7 @@ catch (SmartyException $e)
     dump($smarty->getTemplateVars());
 }
 
-if (1 or ADMIN_SMARTY_LOG_VARS)
+if (0 or ADMIN_SMARTY_LOG_VARS)
 {
     // dump($flatDepartments);
     dump($smarty->getTemplateVars('current_jsonColumnsFileName'));
