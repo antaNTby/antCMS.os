7606b3af2db53e1cf32c33f45723d30e824ad644
insurgeNT@home <antaNT.pro@gmail.com>
2024-05-14 10:36:49 +0300

    default tpl

--- admin/core/departments.php
+++ admin/core/departments.php
@@ -30,9 +30,9 @@ foreach ($Departments as $dpt_key => $dep)
         $value['DPT_SUB'] = $dep_id . '_' . $sub_id; //'trade_orders';
 
         $value['table_columns'] = PATH_CONFIGS . $dep_id . '_' . $sub_id . '__columns.json';
-        $value['sub_processor']     = PATH_INCLUDES . $dep_id . '_' . $sub_id . '.php';
-        $value['sub_template']      = PATH_TPL .'sub/'. $dep_id . '_' . $sub_id . '.tpl.html';
-        $value['sub_href']          = ADMIN_FILE . "?dpt={$dep_id}&sub=" . $sub_id;
+        $value['sub_processor'] = PATH_INCLUDES . $dep_id . '_' . $sub_id . '.php';
+        $value['sub_template']  = PATH_TPL . 'sub/' . $dep_id . '_' . $sub_id . '.tpl.html';
+        $value['sub_href']      = ADMIN_FILE . "?dpt={$dep_id}&sub=" . $sub_id;
 
         if (array_key_exists($sub_id, $subsTables))
         {
@@ -47,18 +47,13 @@ foreach ($Departments as $dpt_key => $dep)
     }
 
     $Departments[$dpt_key]['sub_departments'] = $sub_departments;
-    $Departments[$dpt_key]['sub_count'] = count($dep['sub_departments']);
+    $Departments[$dpt_key]['sub_count']       = count($dep['sub_departments']);
 
 }
 
-
-$Departments_count=count($Departments);
-$Departments_type=gettype($Departments[0]["id"]);
-
-
+$Departments_count = count($Departments);
+$Departments_type  = gettype($Departments[0]["id"]);
 
 $smarty->assign('Departments', $Departments);
 $smarty->assign('Departments_count', $Departments_count);
 // $smarty->assign('Departments_type', $Departments_type);
-
-
--- admin/core/includes/default_dpt.php
+++ admin/core/includes/default_dpt.php
@@ -1,14 +1,5 @@
 <?php
 ###default_dpt.php
-// dump("default_dpt.php PHPFILENAME ".$phpFileName);
-//
-
-
-            if (file_exists($sub_template))
-            {
-                $smarty->assign('admin_main_content_template', $sub_template);
-            }
-
 
 
 
