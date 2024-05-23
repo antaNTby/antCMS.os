5baa25ae9522e0f1ddfd0b5f949bbbdb7c904400
insurgeNT@home <antaNT.pro@gmail.com>
2024-05-23 11:38:11 +0300

    s

--- admin/core/includes/conf_columns.php
+++ admin/core/includes/conf_columns.php
@@ -2,13 +2,14 @@
 ###default_dpt.php
 use RedBeanPHP\R;
 
-
 $page_message      = '';
 $page_message_type = 'success';
 $department        = $Departments[$current_dpt_index];
 $department_sub    = $department['sub_departments'][$current_sub_index];
 
-dump($department_sub["name"]);
+// dump($department_sub["name"]);
+//
+//
 
 $smarty->assign('page_message', $page_message);
 $smarty->assign('page_message_type', $page_message_type);
@@ -16,4 +17,3 @@ $smarty->assign('page_message_type', $page_message_type);
 $smarty->assign('php_department', $department);
 $smarty->assign('php_sub', $department_sub);
 
-    dump($smarty->getTemplateVars());
