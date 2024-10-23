96051a7093f507c9bbb0d8752cbcac0843e1f6c5
Alexander Dell <antaNT.pro@gmail.com>
2024-10-23 22:37:50 +0300

    Boilerplate

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-Здесь будет город-сад
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Здесь будет город-сад
+</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<a href="/admin/admin.php" title="">admin</a>
+</body>
+</html>
