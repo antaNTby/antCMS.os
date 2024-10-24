a87732e4a4be6ac9fe12d89a7546d6ea68352045
Alexander Dell <antaNT.pro@gmail.com>
2024-10-24 09:04:19 +0300

    fonts

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Здесь будет город-сад
-</title>
-	<link rel="stylesheet" href="">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Здесь будет город-сад
+    </title>
+    <link rel="stylesheet" href="">
 </head>
+
 <body>
+    <pre>
 
 	1. Package Control ‣ Install Package ‣ Theme - Monokai Pro
 2. Command Palette ‣ Monokai Pro: select theme
@@ -15,6 +18,52 @@
 
 Theme - Monokai Pro
 https://maximilionus.github.io/monokai_pro_keygen/
+
+
+    // "font_face": "Fira Code",
+    // "font_face": "Comic Code",
+    // "font_face": "Operator Mono",
+    // "font_face": "JetBrains Mono",
+    // "font_face": "Iosevka",
+    // "font_face": "Consolas",
+    // "font_face": "Intel One Mono",
+    // "font_options": [
+    //     "ss01",
+    //     "ss02",
+    //     "ss03",
+    //     "ss11",
+    //     "ss12",
+    //     "locl",
+    //     "mark",
+    //     "numr",
+    //     "dnom",
+    //     "sups",
+    //     "subs",
+    //     "sinf",
+    //     "aalt"
+    // ],
+    // "font_face": "MonoLisa-Medium",
+    // "font_face": "MonoLisa-Regular",
+    // "font_face": "MonaspaceArgon",
+
+    // "color_scheme": "Mariana.sublime-color-scheme",
+    // "color_scheme": "Breakers.sublime-color-scheme",
+    // "color_scheme": "Bass Bowtruckle.sublime-color-scheme",
+    // "color_scheme": "Dark Knight.sublime-color-scheme",
+
+
+    "index_files": false,
+    "theme": "Default Dark.sublime-theme",
+    // "color_scheme": "Dark Knight.sublime-color-scheme",
+    // "color_scheme": "Mariana.sublime-color-scheme",
+
+    // "dark_color_scheme": "Mariana.sublime-color-scheme",
+    // "light_color_scheme": "Mariana.sublime-color-scheme",
+
+
+
 	<a href="/admin/admin.php" title="">admin</a>
+</pre>
 </body>
+
 </html>
