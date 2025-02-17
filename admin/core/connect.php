e5affdd8f8529b8e0dceb1f8ce6b82ef97440a3c
antWorkPC <antaNT.pro@gmail.com>
2025-02-17 17:35:27 +0300

    http://pma5:8000/index.php?route=/database/structure&db=db_antCMS

--- admin/core/connect.php
+++ admin/core/connect.php
@@ -26,6 +26,7 @@
 const DBMS = 'mysqli';       //   {$smarty.const.DBMS}          database host
 const DB_HOST = 'MySQL-5.7'; //   {$smarty.const.DB_HOST}       username
 const DB_USER = 'root';      //   {$smarty.const.DB_USER}       database name
-const DB_PASS = 'root';      //   {$smarty.const.DB_PASS}       database prefix
+// const DB_PASS = 'root';      //   {$smarty.const.DB_PASS}       database prefix
+const DB_PASS = '';      //   {$smarty.const.DB_PASS}       database prefix
 const DB_NAME = 'db_antCMS'; //   {$smarty.const.DB_NAME}       password
 const DB_PRFX = 'ant_';
