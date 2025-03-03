0a208d1598f6c8b1c733081f16a825dd1553acea
antHome <antaNT.pro@gmail.com>
2025-03-03 11:08:10 +0300

    ааа

--- admin/core/bootstrap.php
+++ admin/core/bootstrap.php
@@ -3,6 +3,7 @@ use Smarty\Smarty;
 use Tracy\Debugger;
 
 // https://profitweb.net/resources/phpmyadmin-dlja-open-server-panel-6-0-0-nastrojki-menju.9/
+
 # The VarDumper component provides mechanisms for extracting the state out of any PHP variables.
 # Built on top, it provides a better dump() function that you can use instead of var_dump
 
